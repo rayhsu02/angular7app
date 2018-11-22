5dd53445cd670c00c155e0246de5008e10fa9ef8
lichih hsu <rayhsu02@gmail.com>
2018-11-22 00:08:52 -0800

    init

---
+++ src/app/posts/posts.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-posts',
+  templateUrl: './posts.component.html',
+  styleUrls: ['./posts.component.scss']
+})
+export class PostsComponent implements OnInit {
+
+  post$: Object;
+
+  constructor(private data: DataService) { }
+
+  ngOnInit() {
+    this.data.getPosts().subscribe(
+      data => this.post$ = data
+    )
+  }
+
+}
